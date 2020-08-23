48c2d8694606e6d1f359b683146670a55714083e
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:26:43 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,23 @@
+package typeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int x = 4;
+//		
+//		double y = x;
+//		System.out.println(y);
+//		System.out.println(x);
+	
+	double myDouble = 2.8462;
+	
+	int myInt = (int)myDouble;
+	
+	System.out.println(myDouble);
+	System.out.println(myInt);
+
+	}
+
+}
+ 
